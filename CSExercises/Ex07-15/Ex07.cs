5970a76c684778e62ee887b128dd08a338de31d8
HTINLINAUNG <e0001335@u.nus.edu>
2015-08-16 15:57:18 +0800

    Ex07

--- CSExercises/Ex07-15/Ex07.cs
+++ CSExercises/Ex07-15/Ex07.cs
@@ -10,7 +10,13 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Please Enter Employee's Salary");
+            double salary = Convert.ToDouble (Console.ReadLine());
+            double housingAllowance ,TransportAllowance;
+            housingAllowance=((salary/100)*10);
+            TransportAllowance = ((salary / 100) * 3);
+            Console.WriteLine("The total income is"+" " +(salary+housingAllowance+TransportAllowance));
+           
         }
     }
 }

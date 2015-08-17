d1d7c642dbe06660aff836d9b38e49cf491f2f94
HTINLINAUNG <e0001335@u.nus.edu>
2015-08-17 10:59:09 +0800

    Finished

--- CSExercises/Ex16-20/Ex16.cs
+++ CSExercises/Ex16-20/Ex16.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 namespace CSExercises
 {
     public class Ex16
@@ -5,6 +10,25 @@ namespace CSExercises
         public static void Main(string[] args)
         {
             
+            Console.WriteLine("Enter Your Name");
+            string name =Convert.ToString( Console.ReadLine());
+            Console.WriteLine("Enter Your Sex");
+            string sex = Convert.ToString(Console.ReadLine());
+           
+            {
+                if (sex == "M")
+                {
+                    Console.WriteLine("Good Morning  \t Mr"+name);
+                
+                }
+
+                else if (sex == "F")
+                {
+                    Console.WriteLine("Good Morning \t Ms"+name);
+                }
+            
+            }
+          
         }
     }
 }

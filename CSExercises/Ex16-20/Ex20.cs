813d7eb4270a180b3cd3bb9b32a4e0b747022f75
HTINLINAUNG <e0001335@u.nus.edu>
2015-08-17 13:48:22 +0800

    Finsihed Ex 20

--- CSExercises/Ex16-20/Ex20.cs
+++ CSExercises/Ex16-20/Ex20.cs
@@ -1,10 +1,35 @@
-﻿namespace CSExercises
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace CSExercises
 {
     public class Ex20
     {
         public static void Main(string[] args)
         {
-            
+            int TV, DVD, MP3, Total;
+            int dismp3;
+            Console.WriteLine("Enter the Quantitiy of the TV ");
+            TV = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Quantity of the DVD");
+            DVD = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Quantity of the MP3");
+            MP3 = Convert.ToInt32(Console.ReadLine());
+            dismp3 = (700 * MP3);
+            Total = ((TV * 900) + (DVD * 500));
+            if (Total > 5000)
+            {
+                Console.WriteLine("The Total Price of the Order is  \t" + ((Total * 0.90) + dismp3));
+            }
+            else if (Total > 10000)
+            {
+                Console.WriteLine("The Total Price of the Order is \t" +( (Total * 0.85) + dismp3));
+            }
+
+
         }
     }
 }
+

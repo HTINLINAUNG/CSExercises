5299452705fac0969903adea8d83caa0fa9edf94
HTINLINAUNG <e0001335@u.nus.edu>
2015-08-17 11:26:53 +0800

    Finished Ex 18

--- CSExercises/Ex16-20/Ex18.cs
+++ CSExercises/Ex16-20/Ex18.cs
@@ -1,10 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 namespace CSExercises
 {
     public class Ex18
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Enter Your Mark");
+            int Mark = Convert.ToInt32(Console.ReadLine());
+            if (Mark>=80 && Mark <=100)
+            {
+                Console.WriteLine("Your Grade is \t  A");
             
+            }
+            else if (Mark >= 60 && Mark <= 79)
+            {
+                Console.WriteLine("Your Grade is \t  B");
+            }
+            else if (Mark >= 40 && Mark <= 59)
+            {
+                Console.WriteLine("Your Grade is \t C");
+            }
+            else if (Mark >= 0 && Mark <= 40)
+            {
+                Console.WriteLine("Your Grade is \t  F");
+
+            }
+            else  if (Mark<0)
+            {
+                Console.WriteLine("Your Grade is \t  Error");
+            }
+            else if (Mark > 100)
+            {
+                Console.WriteLine("Your Grade is \t Error");
+            }
+
+
+
+
         }
     }
 }

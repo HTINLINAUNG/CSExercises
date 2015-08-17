e1a26093df4b76e6652e32274d31265e3d293db2
HTINLINAUNG <e0001335@u.nus.edu>
2015-08-17 11:15:38 +0800

    Finished

--- CSExercises/Ex16-20/Ex17.cs
+++ CSExercises/Ex16-20/Ex17.cs
@@ -1,10 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 namespace CSExercises
 {
     public class Ex17
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter Your Name");
+            string Name = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter Your Sex");
+            String Sex = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter Your Age");
+            double age = Convert.ToDouble(Console.ReadLine());
+            if (age >= 40 && Sex == "M")
+            {
+                Console.WriteLine("Good Morning \t  Uncle" + Name);
+
+            }
+            else if (age<40 && Sex == "M")
+
+            {
+                Console.WriteLine("Good Morning \t Mr" + Name);
+            }
+            else if (age >= 40 && Sex == "F")
+            {
+                Console.WriteLine("Good Morning \t Aunty" + Name);
+            }
+            else if (age < 40 && Sex == "F")
+            {
+                Console.WriteLine("Good Morning \t Ms" + Name);
+            }
+
         }
     }
 }

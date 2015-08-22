2091c5334fc4961d10d6a20eda1b36cb247635d6
HTINLINAUNG <e0001335@u.nus.edu>
2015-08-22 13:48:00 +0800

    Complete finished

--- CSExercises/Ex07-15/Ex15.cs
+++ CSExercises/Ex07-15/Ex15.cs
@@ -9,23 +9,29 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Please input a 3-digit integer:");
+            int a = Convert.ToInt32(Console.ReadLine());
 
 
- 
-            Console.WriteLine("Enter x: ");
-            double x = Convert.ToDouble(Console.ReadLine());
+            if (a > 99 && a < 1000)
+            {
 
-            Console.WriteLine("Enter y: ");
-            double y = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Enter z: ");
-            double z = Convert.ToDouble(Console.ReadLine());
-
-            string input = x + "" + y + "" + z;
-            double armstrong = Math.Pow(x, 3) + Math.Pow(y, 3) + Math.Pow(z, 3);
-            if (Convert.ToDouble(input) == armstrong)
-                Console.WriteLine(" That is armstrong number");
-            else Console.WriteLine("That is not armstrong number");
+                int a1 = a / 100;
+                int a2 = (a - a1 * 100) / 10;
+                int a3 = (a - a1 * 100 - a2 * 10);
+                if (a == Math.Pow(a1, 3) + Math.Pow(a2, 3) + Math.Pow(a3, 3))
+                {
+                    Console.WriteLine("{0} is Armstrong Number", a);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is not Armstrong Number", a);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Make sure Your number is ArmStrong Number");
+            }
         }
     }
 }

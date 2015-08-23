2abe3b17cb320857f005c605aeadb81e1596e382
HTINLINAUNG <e0001335@u.nus.edu>
2015-08-23 15:23:08 +0800

    Finished

--- CSExercises/Ex16-20/Ex20.cs
+++ CSExercises/Ex16-20/Ex20.cs
@@ -9,24 +9,29 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            int TV, DVD, MP3, Total;
-            int dismp3;
-            Console.WriteLine("Enter the Quantitiy of the TV ");
-            TV = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the Quantity of the DVD");
-            DVD = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the Quantity of the MP3");
-            MP3 = Convert.ToInt32(Console.ReadLine());
-            dismp3 = (700 * MP3);
-            Total = ((TV * 900) + (DVD * 500));
-            if (Total > 5000)
+
+            Console.WriteLine("Please enter the TV Product");
+            int tv_qty = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the DVD Produc");
+            int dvd_qty = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the MP3 Amount");
+            int Mp3_qty = Convert.ToInt32(Console.ReadLine());
+            double TV_Price = tv_qty * 900;
+            double Dvd_Price = dvd_qty * 500;
+            double Mp3_Price = Mp3_qty * 700;
+            double Total_Price = TV_Price + Dvd_Price + Mp3_Price;
+            if (Total_Price > 5000 && Total_Price < 10000)
             {
-                Console.WriteLine("The Total Price of the Order is  \t" + ((Total * 0.90) + dismp3));
+                TV_Price = TV_Price * 0.9;
+                Dvd_Price = Dvd_Price * 0.9;
             }
-            else if (Total > 10000)
+            else if (Total_Price > 10000)
             {
-                Console.WriteLine("The Total Price of the Order is \t" +( (Total * 0.85) + dismp3));
+                TV_Price = TV_Price * 0.85;
+                Dvd_Price = Dvd_Price * 0.85;
             }
+            Total_Price = TV_Price + Dvd_Price + Mp3_Price;
+            Console.WriteLine("Total price of this order is {0:c}", Total_Price);
 
 
         }
